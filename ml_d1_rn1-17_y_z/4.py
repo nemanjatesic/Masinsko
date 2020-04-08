a79925426a1e223a5fdabfe6f30648a1657470cd
Nemanja <ntesic17@raf.rs>
2020-04-08 02:39:11 +0200

    Always same output

--- ml_d1_rn1-17_y_z/4.py
+++ ml_d1_rn1-17_y_z/4.py
@@ -43,7 +43,7 @@ class MultinomialNaiveBayes:
     def add_feature_vector(self, feature_vector, classPN):
         # Racunamo broj pojavljivanja svake reci u svakoj klasi
         self.numberOfFeatures += 1
-        if classPN == 0:
+        if classPN == 1:
             self.numberOfPositive += 1
         else:
             self.numberOfNegative += 1
@@ -53,7 +53,7 @@ class MultinomialNaiveBayes:
 
     def add_feature_vector_tmp(self, feature_vector, classPN):
         self.numberOfFeatures += 1
-        if classPN == 0:
+        if classPN == 1:
             self.numberOfPositive += 1
         else:
             self.numberOfNegative += 1
@@ -67,7 +67,7 @@ class MultinomialNaiveBayes:
         # broja u intervalu [0, maksimalni broj u listi]
         print(self.occurrences[0])
         print(self.occurrences[1])
-        self.priors = np.asarray([self.numberOfPositive/self.numberOfFeatures, self.numberOfNegative/self.numberOfFeatures])
+        self.priors = np.asarray([self.numberOfNegative/self.numberOfFeatures, self.numberOfPositive/self.numberOfFeatures])
         print('Priors:')
         print(self.priors)
 
@@ -226,19 +226,6 @@ if len(vocab) > 15000:
 print('Feature vector size: ', len(vocab))
 model = MultinomialNaiveBayes(nb_classes=2, nb_words=len(vocab), pseudocount=1)
 
-# Bag of Words model
-# print('Creating BOW features...')
-# X = np.zeros((len(clean_corpus), len(vocab)), dtype=np.float32)
-# for doc_idx in range(len(clean_corpus)):
-#     doc = clean_corpus[doc_idx]
-#     for word_idx in range(len(vocab)):
-#         word = vocab[word_idx]
-#         cnt = numocc_score(word, doc)
-#         X[doc_idx][word_idx] = cnt
-
-
-######################################################################
-
 random_index = create_random_indexes(max)
 
 # Bag of Words model
@@ -266,22 +253,11 @@ for i in range(int(max*0.8)):
 
     model.add_feature_vector_tmp(new_feature_vector, data['y'][doc_idx])
 
-
-    # new_feature_vector = [0 for i in range(feature_vector_size)]
-    # for word_idx in range(feature_vector_size):
-    #     word = vocab[word_idx]
-    #     cnt = numocc_score(word, doc)
-    #     new_feature_vector[word_idx] = cnt
-    # if i % broj == 0:
-    #     print(tmp, '%')
-    #     tmp += 5
-    # model.add_feature_vector(new_feature_vector, data['y'][doc_idx])
-
 model.fit()
 
 print('Checking for test set...')
 brojTacnih = 0
-class_names = ['Positive', 'Negative']
+class_names = ['Negative', 'Positive']
 for i in range(int(max*0.8), max):
     doc_idx = random_index[i]
     doc = clean_corpus[doc_idx]
@@ -296,66 +272,12 @@ for i in range(int(max*0.8), max):
         number_of_occ = numocc_score(word, doc)
         new_feature_vector.append((vocab_dic.get(word, -1), number_of_occ))
 
-    # new_feature_vector = [0 for i in range(feature_vector_size)]
-    # for word_idx in range(feature_vector_size):
-    #     word = vocab[word_idx]
-    #     cnt = numocc_score(word, doc)
-    #     new_feature_vector[word_idx] = cnt
-    # if i % broj == 0:
-    #     print(tmp, '%')
-    #     tmp += 5
-    # prediction = model.predict(np.asarray(new_feature_vector))
     prediction = model.predict_tmp(new_feature_vector)
-    if class_names[prediction] == 'Positive' and data['y'][doc_idx] == 0:
+    if class_names[prediction] == 'Positive' and data['y'][doc_idx] == 1:
         brojTacnih += 1
-    if class_names[prediction] == 'Negative' and data['y'][doc_idx] == 1:
+    if class_names[prediction] == 'Negative' and data['y'][doc_idx] == 0:
         brojTacnih += 1
 
 tmpBr = brojTacnih / int(max * 0.2) * 100
-
 print('Procent pogodjenih : ', tmpBr)
-
-######################################################################
-
-# f.close()
-
-
-# # creating sets for learning and testing
-# print('Creating sets for learning and testing...')
-#
-# YLearning = []
-# XLearning = []
-# for i in range(int(max * 0.8)):
-#     index = random_index[i]
-#     YLearning.append(data['y'][index])
-#     XLearning.append(X[index])
-#
-# YTest = []
-# XTest = []
-# for i in range(int(max * 0.8), max):
-#     index = random_index[i]
-#     YTest.append(data['y'][index])
-#     XTest.append(X[index])
-#
-# class_names = ['Positive', 'Negative']
-# YLearningNP = np.asarray(YLearning)
-# XLearningNP = np.asarray(XLearning)
-#
-# print('Starting to fit...')
-#
-# model.fit(XLearningNP, YLearningNP)
-#
-# print('Checking for test set...')
-# brojTacnih = 0
-# for i in range(len(YTest)):
-#     prediction = model.predict(np.asarray(XTest[i]))
-#     if class_names[prediction] == 'Positive' and YTest[i] == 0:
-#         brojTacnih += 1
-#     if class_names[prediction] == 'Negative' and YTest[i] == 1:
-#         brojTacnih += 1
-#
-# tmpBr = brojTacnih / int(max * 0.2) * 100
-#
-# print('Procent pogodjenih : ', tmpBr)
-
 print('done')
